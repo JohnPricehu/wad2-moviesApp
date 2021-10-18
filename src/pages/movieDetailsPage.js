5deeb427148687b228eb881d8d0c668a61133556
Yiming Hu <20092158@mail.wit.ie>
2021-10-18 12:37:46 +0100

    Show movie review excerpts; Refactor Details page.

--- src/pages/movieDetailsPage.js
+++ src/pages/movieDetailsPage.js
@@ -1,41 +1,32 @@
-import React, {useState, useEffect}  from "react";
-import MovieHeader from "../components/headerMovie/";
-import MovieDetails from "../components/movieDetails/";
-import Grid from "@material-ui/core/Grid";
-import { makeStyles } from "@material-ui/core/styles";
-import GridList from "@material-ui/core/GridList";
-import GridListTile from "@material-ui/core/GridListTile";
-import { getMovie, getMovieImages } from "../api/tmdb-api";
+import React, { useState, useEffect } from "react";
+import { withRouter } from "react-router-dom";
+import MovieDetails from "../components/movieDetails";
+import PageTemplate from "../components/templateMoviePage";
+import { getMovie } from "../api/tmdb-api";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "space-around",
-  },
-  gridList: {
-    width: 450,
-    height: "100vh",
-  },
-}));
-
-const MoviePage = (props) => {
-  const classes = useStyles();
+const MovieDetailsPage = (props) => {
   const { id } = props.match.params;
   const [movie, setMovie] = useState(null);
-  const [images, setImages] = useState([]);
+
   useEffect(() => {
     getMovie(id).then((movie) => {
       setMovie(movie);
     });
   }, [id]);
 
-  useEffect(() => {
-    getMovieImages(id).then((images) => {
-      setImages(images);
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  return (
+    <>
+      {movie ? (
+        <>
+          <PageTemplate movie={movie}>
+            <MovieDetails movie={movie} />
+          </PageTemplate>
+        </>
+      ) : (
+        <p>Waiting for movie details</p>
+      )}
+    </>
+  );
 };
 
-export default MoviePage;
+export default withRouter(MovieDetailsPage);
