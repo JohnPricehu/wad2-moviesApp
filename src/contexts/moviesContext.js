9ca1a093678ceaecd10898466a6e6d0917f96353
Yiming Hu <20092158@mail.wit.ie>
2021-11-01 12:32:17 +0000

    Add new review support.

--- src/contexts/moviesContext.js
+++ src/contexts/moviesContext.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 export const MoviesContext = React.createContext(null);
 
 const MoviesContextProvider = (props) => {
+  const [myReviews, setMyReviews] = useState( {} ) 
   const [favorites, setFavorites] = useState( [] )
 
   const addToFavorites = (movie) => {
@@ -14,13 +15,17 @@ const MoviesContextProvider = (props) => {
       (mId) => mId !== movie.id
     ) )
   };
+  const addReview = (movie, review) => {
+    setMyReviews( {...myReviews, [movie.id]: review } )
+  };
 
-  return (
+ return (
     <MoviesContext.Provider
       value={{
         favorites,
         addToFavorites,
-        removeFromFavorites
+        removeFromFavorites,
+        addReview,
       }}
     >
       {props.children}
