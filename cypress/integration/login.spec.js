7565b3fc341b40ec2bf3626e4743ac078f0d23cf
Yiming Hu <20092158@mail.wit.ie>
2021-12-12 19:06:40 +0000

    E2E tests - login page - add wrong input test

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -9,6 +9,11 @@ describe("Login Page ", () => {
       cy.get('input').eq(1).type('123456') 
       cy.get('button').eq(8).click()  //click the button    
     })
-    
-  
+    it("if input wrong username or password,it returns Login Fail", () => {
+      cy.get('button').eq(7).click()
+      cy.get('input').eq(0).type('34@gmail.com')   //type the wrong user name in the field
+      cy.get('input').eq(1).type('123456') 
+      cy.get('button').eq(8).click()
+      cy.get('.card').find('div').contains("Login Fail!") 
+    })
   });
