2d9949cb37050cb098e79908dc9d42262ecd457e
Yiming Hu <20092158@mail.wit.ie>
2021-12-12 22:28:43 +0000

    Fix some problems

---
+++ src/pages/loginPage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import AuthProvider from '../../src/contexts/authContext'
+import LogInForm from '../../src/components/login'
+
+export default function LogInPage() {
+    return (
+        <AuthProvider>
+        <Container className = "d-flex align-items-center justify-content-center" 
+        style={ {minHeight: "100vh"}}
+        >
+            <div className = "w-100" style ={{maxWidth:"400px"}}>
+            <LogInForm/>
+            </div>
+        </Container>
+        </AuthProvider>
+    )
+} 
