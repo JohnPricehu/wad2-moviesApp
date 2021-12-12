ac23b1b116d922be84d52bff8aea35f462bce669
Yiming Hu <20092158@mail.wit.ie>
2021-12-12 20:11:49 +0000

    E2E tests - favourite page - delete favourite movie test

---
+++ cypress/integration/favourite-page.spec.js
@@ -0,0 +1,54 @@
+let movies;    // List of movies from TMDB
+
+// Utility functions
+const filterByTitle = (movieList, string) =>
+    movieList.filter((m) => m.title.toLowerCase().search(string) !== -1);
+
+const filterByGenre = (movieList, genreId) =>
+    movieList.filter((m) => m.genre_ids.includes(genreId));
+
+const filterByTitleAndGenre =(moveList,string,genreId)=>
+    moveList.filter((m)=> m.title.toLowerCase().search(string) !== -1 && m.genre_ids.includes(genreId));
+
+
+describe("Favourite Page", () => {
+    before(() => {
+        // Get movies from TMDB and store in movies variable.
+        cy.request(
+            `https://api.themoviedb.org/3/discover/movie?api_key=${Cypress.env(
+                "TMDB_KEY"
+            )}&language=en-US&include_adult=false&include_video=false&page=1`
+        )
+            .its("body")    // Take the body of HTTP response from TMDB
+            .then((response) => {
+                movies = response.results
+            })
+    })
+    beforeEach(() => {
+        cy.visit("/")
+    });
+
+    describe("Base test", () => {
+        it("displays page header", () => {
+            cy.get("Button").eq(2).click();
+            cy.get("h3").contains("Favorite Movies");
+        });
+    })
+    describe("delete favourite movie", () => {
+        it("should delete the selected movies on the Favourites page", () => {  
+          cy.get("button[aria-label='add to favorites']").eq(0).click({force: true});
+          cy.get("button[aria-label='add to favorites']").eq(2).click({force: true});
+          cy.get(".MuiAvatar-root").should("have.length",2);  
+          cy.get("header").find(".MuiToolbar-root").find("button").eq(2).click();
+          cy.get(".MuiCardHeader-content").should("have.length",2);
+          cy.get(".MuiCardHeader-content").eq(0).find("p").contains(movies[0].title)
+          cy.get(".MuiCardHeader-content").eq(1).find("p").contains(movies[2].title)
+          cy.get("button[aria-label='remove from favorites']").eq(0).click();
+          cy.get("button[aria-label='remove from favorites']").eq(0).click();
+          cy.get(".MuiAvatar-root").should("have.length",0); 
+        });
+      });
+
+
+
+})
